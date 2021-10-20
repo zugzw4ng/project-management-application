46e9f40bb4bacaaacfc718629d4fe2a84903b697
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-20 13:19:45 -0400

    fixed typo

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        processs.env.DB_PASSWORD,
+        process.env.DB_PASSWORD,
     {
         host: 'localhost',
         dialect: 'mysql',
