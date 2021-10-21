21a1f99efbf9084820d787757ad1cd094fe9d19c
Nicole Pingar <nicolepingar@yahoo.com>
2021-10-21 12:54:06 -0400

    prfile.handlebars work

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,3 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems);
 });
+
+const newFormHandler = async (event) => {
+    event.preventDefault();
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+});
